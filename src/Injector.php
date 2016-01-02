1dac2d0c75d768acac9b955414927d68b66a668a
Antonis Pavlakis <antonis@pavlakis.info>
2016-01-02 14:37:19 +0000

    Updated copyright to 2016.

--- src/Injector.php
+++ src/Injector.php
@@ -3,7 +3,7 @@
  * A semi-automatic DI resolver retrieving dependencies through a container.
  *
  * @link      https://github.com/pavlakis/seaudi
- * @copyright Copyright © 2015 Antonis Pavlakis
+ * @copyright Copyright © 2015-2016 Antonis Pavlakis
  * @license   https://github.com/pavlakis/seaudi/blob/master/LICENSE (BSD 3-Clause License)
  */
 
